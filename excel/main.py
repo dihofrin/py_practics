a577375c6f9306def35a38df8fd85bd81d7c7aec
dihofrin <serhiopich@gmail.com>
2022-06-23 23:28:45 +0300

    finished example

--- excel/main.py
+++ excel/main.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import openpyxl
 from openpyxl import load_workbook
 from openpyxl.styles import Font
 from openpyxl.chart import BarChart, Reference
@@ -24,9 +23,6 @@ max_row = wb.active.max_row
 
 # adding diagrams to excel (using module Barchart from openpyxl)
 
-wb = load_workbook('report_2021.xlsx')
-sheet = wb['Report']
-
 # barchart
 barchart = BarChart()
 
@@ -39,15 +35,31 @@ categories = Reference(sheet, min_col=min_column, max_col=min_column, min_row=mi
 
 # adding data and categories
 
-barchart.add_data(data, titles_from_data=True)
+barchart.add_data(data, titles_from_data=True) # adding headers for data
 barchart.set_categories(categories)
 
 # location chart
-sheet.add_chart(barchart, "B12")
+sheet.add_chart(barchart, "B12") # adding histogram
 barchart.title = 'Sales by Product line'
-barchart.style = 5 # choose the chart style
+barchart.style = 10 # choose the chart style
 
-sheet['B7'] = '=SUM(B5:B6)'
-sheet['B7'].style = 'Currency'
+alphabet = list(string.ascii_uppercase)
+excel_alphabet = alphabet[0:max_column]
+
+# sum in columns B-G
+for i in excel_alphabet:
+    if i != 'A':
+        sheet[f'{i}{max_row+1}'] = f'=SUM({i}{min_row+1}:{i}{max_row})'
+        sheet[f'{i}{max_row+1}'].style = 'Currency'
+
+# adding total label
+sheet[f'{excel_alphabet[0]}{max_row+1}'] = 'Total'
+
+# adding formatting
+
+sheet['A1'] = 'Sales Report'
+sheet['A2'] = '2021'
+sheet['A1'].font = Font('Arial', bold=True, size=20)
+sheet['A2'].font = Font('Arial', bold=True, size=10)
 wb.save('report_2021.xlsx')
 
